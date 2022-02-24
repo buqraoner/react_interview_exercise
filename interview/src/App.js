4125d3b11e83760ab8fb862e9bd6be8d2ea8ffb1
Buğra Öner <boletroy@gmail.com>
2022-02-24 18:41:40 +0300

    Final

--- interview/src/App.js
+++ interview/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 //Components
+import UserList from './components/UserList';
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
     <Header/>
+    <UserList/>
     </>
     
     
--- interview/src/components/UserList.jsx
+++ interview/src/components/UserList.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import { React, useState } from "react";
 import axios from 'axios';
-import { useState, UseEffect } from "react";
+import { useEffect } from "react";
+//Components
+import SingleUser from "./SingleUser";
+
 
 
 
 const UserList = () => {
     const [users, setUsers] = useState([]);
-
-
-    const fetchUser= async()=>{
-     const response =   await axios.get("https://randomuser.me/api/?results=5")
+    const fetchUser = async () => {
+        const response = await axios.get("https://randomuser.me/api/?results=5")
+        setUsers(response.data.results)
     }
 
-    setUsers(response.data)
+   
 
-    return <div></div>
+    useEffect(()=>
+    {
+        fetchUser()
+    },[])
+
+    return (
+    <div className="user-list">
+        <ul>
+            {users.map((user,idx)=>(            
+                <li>
+                    <SingleUser user={user} key={idx}/>
+                </li>
+            ))}
+        </ul>
+    </div>
+    )
 }
 
 
